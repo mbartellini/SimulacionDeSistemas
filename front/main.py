c42dd99d409af8a9b64ca4a6f21e47f2d5d55282
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-08-10 19:27:35 -0300

    Add basic main script

--- front/main.py
+++ front/main.py
@@ -1,8 +1,18 @@
+from particle import Particle
 from utils.parser import *
 
 
 def main():
-    particle_count, grid_size, properties = parse_static("Static100.txt")
+    particle_count, grid_size, static_properties = parse_static("Static100.txt")
+    time, dynamic_data = parse_dynamic("Dynamic100.txt", particle_count)
+    particles = []
+    for p in range(particle_count):
+        particles.append(Particle(
+            dynamic_data[p][0],
+            dynamic_data[p][1],
+            static_properties[p][0],
+            static_properties[p][1]
+        ))
 
 
 if __name__ == '__main__':
