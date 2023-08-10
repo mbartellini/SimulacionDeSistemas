c557eb16aded5f81f8366cefd6b10e4406662172
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-08-10 19:27:09 -0300

    Parse dunamic file

--- front/utils/parser.py
+++ front/utils/parser.py
@@ -4,9 +4,19 @@ def parse_static(filename: str):
         particle_count = int(lines[0])
         grid_size = int(lines[1])
         properties_lines = lines[2:]
-        properties = [_parse_static_line(line) for line in properties_lines]
+        properties = [_split_line_into_floats(line) for line in properties_lines]
     return particle_count, grid_size, properties
 
 
-def _parse_static_line(line: str):
+def _split_line_into_floats(line: str):
     return [float(prop) for prop in line.split()] # Should check if we always take 2 props
+
+
+def parse_dynamic(filename: str, particle_count: int): # First iteration, only for
+    with open(filename) as f:
+        lines = f.readlines()
+        time = int(lines[0])
+        data = []
+        for p in range(particle_count):
+            data.append(_split_line_into_floats(lines[p+1]))
+    return time, data
