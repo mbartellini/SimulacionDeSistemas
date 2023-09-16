32d3ae274f20fb7020ffa073fca53b15125c2a18
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-09-16 15:37:38 -0300

    Change format of xyz file

--- TP3/event-driven-simulation/src/main/java/ar/edu/itba/ss/models/Simulation.java
+++ TP3/event-driven-simulation/src/main/java/ar/edu/itba/ss/models/Simulation.java
@@ -117,7 +117,7 @@ public class Simulation {
         try (FileWriter fw = new FileWriter(DYNAMIC, true)) {
             fw.append(String.format("%d\n\n", particles.length));
             for(Particle p : particles) {
-                fw.append(String.format("%d %g %g 0 %g %g 0 %g\n", p.getId(), p.getX(), p.getY(), p.getVx(), p.getVy(), p.getRadius()));
+                fw.append(String.format("%d %g %g %g %g %g\n", p.getId(), p.getX(), p.getY(), p.getVx(), p.getVy(), p.getRadius()));
             }
         } catch (IOException e) {
             System.err.printf("Error writing to %s: %s", DYNAMIC, e.getMessage());
